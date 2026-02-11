22fbe3e82e8113b81b183eef6990f4396589f8af
Michael Bruns <michael.mi.bruns@deutschebahn.com>
2026-02-11 20:34:29 +0100

    Revert "feat: add empty provider config auth values check to prevent late errs"
    This reverts commit 26bf363690f13578f774f75ed830fe6bb5de8660.

--- redshift/config_pq_proxy.go
+++ redshift/config_pq_proxy.go
@@ -52,16 +52,9 @@ func getConfigFromPqResourceData(d *schema.ResourceData, database string, maxCon
 	var err error
 	var password string
 	host := d.Get("host").(string)
-	if host == "" {
-		return nil, fmt.Errorf("host must be specified and non-empty")
-	}
 	username := d.Get("username").(string)
-	if username == "" {
-		return nil, fmt.Errorf("username must be specified and non-empty")
-	}
 	port := d.Get("port").(int)
 	sslMode := d.Get("sslmode").(string)
-	log.Printf("[DEBUG] using username %q for authentication\n", username)
 	_, useTemporaryCredentials := d.GetOk("temporary_credentials")
 	if useTemporaryCredentials {
 		log.Println("[DEBUG] using temporary credentials authentication")
@@ -73,9 +66,6 @@ func getConfigFromPqResourceData(d *schema.ResourceData, database string, maxCon
 	} else {
 		log.Println("[DEBUG] using password authentication")
 		password = d.Get("password").(string)
-		if password == "" {
-			return nil, fmt.Errorf("password must be specified and non-empty when using password authentication")
-		}
 	}
 	return NewPqConfig(host, database, username, password, port, sslMode, maxConnections), nil
 }
